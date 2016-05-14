022b0f965136179832e7e40e178a463531773578
OnurbSilva <brunosilva@MacBook-Pro-de-Bruno.local>
2016-05-14 21:41:01 +0100

    summary

---
+++ Assets/scripts/bunnyControler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bunnyControler : MonoBehaviour {
+
+	private Rigidbody2D myRigidBody;
+	public float bunnyjump = 500;
+
+	// Use this for initialization
+	void Start () {
+
+		myRigidBody = GetComponent<Rigidbody2D>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonUp("Jump")) 
+		{
+
+			myRigidBody.AddForce (transform.up * bunnyjump);
+		}
+			
+	
+	}
+}
