a6e48efb4d9760e34d4658abacb319f3f5772bef
OnurbSilva <brunosilva@MacBook-Pro-de-Bruno.local>
2016-05-16 09:24:16 +0100

    ultimas

---
+++ Assets/scripts/PreFabSpwaner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PreFabSpwaner : MonoBehaviour {
+
+	private float nextSpwan = 0;
+	public Transform prefabToSpawn;
+	public float spawnRate = 1 ;
+	public float randomDelay = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time > nextSpwan) 
+		{
+			Instantiate (prefabToSpawn, transform.position, Quaternion.identity);
+			nextSpwan = Time.time + spawnRate + Random.Range (0, randomDelay);
+		}
+	
+	}
+}
--- Assets/scripts/bunnyControler.cs
+++ Assets/scripts/bunnyControler.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class bunnyControler : MonoBehaviour {
 
 	private Rigidbody2D myRigidBody;
+	private Animator myAnim;
 	public float bunnyjump = 500;
 
 	// Use this for initialization
 	void Start () {
 
 		myRigidBody = GetComponent<Rigidbody2D>();
+		myAnim = GetComponent<Animator>();
 	
 	}
 	
@@ -21,7 +24,17 @@ public class bunnyControler : MonoBehaviour {
 
 			myRigidBody.AddForce (transform.up * bunnyjump);
 		}
-			
+	
+		myAnim.SetFloat ("vVelocity", myRigidBody.velocity.y);
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.collider.gameObject.layer == LayerMask.NameToLayer("enemy")) 
+		{
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+		}
 	
 	}
 }
